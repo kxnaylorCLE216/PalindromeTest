c25606fb706d63b5967ef7b4aec03eaadb01cb24
root <naylorkx@gmail.com>
2019-02-17 10:52:32 -0500

    Made changes so the tests will run on a Chromebook

--- src/test/java/AutomatedTests/AutomatedTests/BeforeAndAfter.java
+++ src/test/java/AutomatedTests/AutomatedTests/BeforeAndAfter.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
@@ -21,7 +20,7 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import data.provider.ConfigReader;
 
 public class BeforeAndAfter {
-	
+
 	 static WebDriver driver;
 	 static pages.LoginPage loginPage;
 	 static pages.PalindromeTestPage palindromeTestPage;
@@ -30,96 +29,103 @@ public class BeforeAndAfter {
 	 static	ConfigReader reader = new ConfigReader();
 	 static final String HEADLESSPROP = "headless";
 	 static String strURL = reader.getURL();
-	 
+
 	 protected BeforeAndAfter() {
 	      //not called
 	   }
-	 	 
+
 	@BeforeClass
-	public static void setUp(){		
-		
+	public static void setUp(){
+
 		String strBrowserType = reader.getBrowserType();
-		
+
 		String strIsHeadLess = reader.getIsHeadLess();
-		
+
 		String strDriverPath = reader.getDriverPath();
-				
+
 		System.setProperty(strBrowserType, strDriverPath);
-			
+
 		System.setProperty(HEADLESSPROP, strIsHeadLess);
-					        
+
         if (strBrowserType.contains("chrome")) {
         	startChromeDriver();
         }
         else {
         	startFireFoxDriver();
         }
-         	               	
+
 		loginPage = new pages.LoginPage(driver);
-		
+
 		palindromeTestPage = new pages.PalindromeTestPage(driver);
-			
+
 	}
-	
+
 	@AfterClass
 	public static void tearDown(){
-		
+
 		 if(null != driver) {
 	            driver.close();
-	          
+
 	        }
-	
+
 	}
-	
+
 	private static void startChromeDriver() {
-		
+
 		ChromeOptions chromeOptions = new ChromeOptions();
-		
+
 		headless = System.getProperty(HEADLESSPROP);
-		
+
 		if(headless.equals("true")) {
             chromeOptions.addArguments("--headless");
         } else {
-        	chromeOptions.addArguments("--disable-infobars");
-        } 
-		
-		driver = new ChromeDriver(chromeOptions);
+					chromeOptions.addArguments("start-maximized"); // open Browser in maximized mode
+					chromeOptions.addArguments("disable-infobars"); // disabling infobars
+					chromeOptions.addArguments("--disable-extensions"); // disabling
+					chromeOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+					chromeOptions.addArguments("--no-sandbox"); // Bypass OS security
+        }
+
+				driver = new ChromeDriver(chromeOptions);
 	}
-	
+
 	private static void startFireFoxDriver() {
-		
+
 		FirefoxOptions ffOptions = new FirefoxOptions();
-		   
+
 		headless = System.getProperty(HEADLESSPROP);
-		
+
 		if(headless.equals("true")) {
             ffOptions.addArguments("--headless");
         } else {
         	ffOptions.addArguments("--disable-infobars");
-        } 
-		
+					ffOptions.addArguments("--disable-extensions"); // disabling
+					ffOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+					ffOptions.addArguments("--no-sandbox"); // Bypass OS security
+        }
+
         driver = new FirefoxDriver(ffOptions);
 	}
-	
+
 	public static void printPalindromeCount(List<String> palindromesList) {
-				
-		 List<String> lines = Arrays.asList("The count of the visible Palindromes", 
+
+		 List<String> lines = Arrays.asList("The count of the visible Palindromes",
 				 String.valueOf(palindromesList.size()));
-		 
+
 		 Path file = Paths.get("countPalindromeNum.txt");
-		 
+
 		 try {
-			 
+
 			Files.write(file, lines, Charset.forName("UTF-8"));
-			
+
 		} catch (IOException e) {
-			
+
 			String message = "Unable to write to countPalindromeNum.txt!";
-			
+
 			Logger logger = Logger.getLogger(BeforeAndAfter.class.getName());
-			
+
 			logger.log(Level.SEVERE, message, e);
-   		
+
 		}
 	}
 }
